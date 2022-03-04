679101ad15f633ee2e6e753357cd225efe416056
Ricardo Mangandi <ricardo.mangandi@gmail.com>
2022-03-04 17:35:40 -0500

    finished part one

--- part1/Party.java
+++ part1/Party.java
@@ -1,13 +1,9 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 
 
 class Labyrnth extends Thread
 {
 
-   // public static AtomicBoolean cupcakeReady = new AtomicBoolean(true);
-   // public static AtomicLong numberOfTimesBroughtCupcakeOut = new AtomicLong(0);
     public static long broughtCakeOut = 0;
     public static boolean cupcakeReady = true;
    
@@ -15,13 +11,12 @@ class Labyrnth extends Thread
     public void run()
     {
         logicToeatCake();
-
     }
 
 
     public synchronized void logicToeatCake()
     {   
-        System.out.println("Guest "+Party.currentGuestInside+" has gone inside the labrynth");
+       
        
         if(!Party.haveVisted[Party.currentGuestInside] && isThereAcupcake())
         {
@@ -31,9 +26,9 @@ class Labyrnth extends Thread
 
         else if(!Party.haveVisted[Party.currentGuestInside] && !isThereAcupcake())
         {
-            long getAsked = askServantForCupcake();
+            long getNumberOfTimesEveryOneHasAskedForCake = askServantForCupcake();
 
-            if (getAsked == Party.numberOfGuests - 1)
+            if (getNumberOfTimesEveryOneHasAskedForCake == Party.numberOfGuests - 1)
             {
                 Party.partyGoesOn = false;
             }
@@ -94,7 +89,7 @@ class Labyrnth extends Thread
 public class Party {
 
     static int numberOfGuests;
-    static Thread [] arrayOfGuestsThreads;
+    static ArrayList<Thread> arrayOfGuestsThreads;
     static boolean [] haveVisted;
     static boolean partyGoesOn = true;
     static int currentGuestInside;
@@ -104,75 +99,55 @@ public class Party {
         Random minotaurPicksGuestsRandomly = new Random();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter how many guests are coming to the party: ");
-        //numberOfGuests =  scanner.nextInt();
-        numberOfGuests = 3;
-        System.out.print(numberOfGuests);
-        System.out.println();
+        numberOfGuests =  scanner.nextInt();
         scanner.close();
         
-        arrayOfGuestsThreads = new Labyrnth[numberOfGuests];
+        //allocate memory
         haveVisted = new boolean[numberOfGuests];
-
-            
-     
+        arrayOfGuestsThreads = new ArrayList<>();
+        
+        //initialize haveVisited Array for logic to reach goal
         for(int i = 0; i < numberOfGuests; i++)
         {
-            Thread aThread = new Labyrnth();
-            arrayOfGuestsThreads[i] = aThread;
             haveVisted[i] = false;
         }
-
-       ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-       int i = 0;
-       while(partyGoesOn)
-        {   
-            currentGuestInside = minotaurPicksGuestsRandomly.nextInt(numberOfGuests);
-            
-            if(arrayOfGuestsThreads[currentGuestInside].getState().toString() == "NEW"){
-                
-                arrayOfGuestsThreads[currentGuestInside].start();
-               
-            }
-
-            else if (arrayOfGuestsThreads[currentGuestInside].getState().toString() == "RUNNABLE"
-                    || arrayOfGuestsThreads[currentGuestInside].getState().toString() == "BLOCKED" ||
-                    arrayOfGuestsThreads[currentGuestInside].getState().toString() == "WAITING" 
-
-
-            )
-            {
-                continue;
-            }
-            
-     
-            else //if(arrayOfGuestsThreads[currentGuestInside].getState().toString() == "TERMINATED")
-            {   
-
-               // System.out.println(arrayOfGuestsThreads[currentGuestInside].getState().toString());
-                arrayOfGuestsThreads[currentGuestInside].run();
-            }
-
-       
-
-            i++;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
         
-        System.out.println("Party status:" + partyGoesOn);
-        System.out.println("Number of times asked servant: "+ Labyrnth.broughtCakeOut);
-        System.out.println(i);
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-       for(Thread atThread : arrayOfGuestsThreads)
-        {   
-            atThread.join();
-        } 
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      //wininning strategy is to find out how many guests there are before the game starts
+      //then when the game does start ask the servant how many times they have brought cake out
+      //when the servant has brought cake out at n - 1 times then we are sure everyone has eaten cake at least one
+      //and has gone through the labrynth at least once. 
          
+       ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       int threadCounter = 0;
+       long partyStartTime = System.nanoTime();
+       if(numberOfGuests > 1)
+       {
+            while(partyGoesOn)
+            {   
+                currentGuestInside = minotaurPicksGuestsRandomly.nextInt(numberOfGuests);
+            
+                arrayOfGuestsThreads.add(new Labyrnth());
+
+                arrayOfGuestsThreads.get(threadCounter).start();
+
+                //the join method allows one thread to wait until another thread completes it execution
+                arrayOfGuestsThreads.get(threadCounter).join();
+            
+                threadCounter++;
+            }
+        }   
+        else
+        {
+            arrayOfGuestsThreads.add(new Labyrnth());
+            arrayOfGuestsThreads.get(0).start();
+            arrayOfGuestsThreads.get(0).join();
+        }
+        
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        long partyEndTime = System.nanoTime();
+        long partyTotalTime = partyEndTime - partyStartTime;
+        System.out.println("The party had " + numberOfGuests + " guests and ended in: "+(partyTotalTime) / (Math.pow(10, 9)) + " seconds");
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }    
 }
 
